492d52219d5b79fb340160cd480ef77279023f9f
lassepitknen <lassepitknen@gmail.com>
2020-07-07 17:35:49 +0300

    test(*): add dep tests

---
+++ test/forwarded.js
@@ -0,0 +1,43 @@
+import assert from 'assert';
+import forwarded from '../lib/deps/forwarded/index.js';
+
+describe('forwarded(req)', function () {
+  it('should require req', function () {
+    assert.throws(forwarded.bind(null), /argument req.*required/)
+  })
+
+  it('should work with X-Forwarded-For header', function () {
+    var req = createReq('127.0.0.1')
+    assert.deepEqual(forwarded(req), ['127.0.0.1'])
+  })
+
+  it('should include entries from X-Forwarded-For', function () {
+    var req = createReq('127.0.0.1', {
+      'x-forwarded-for': '10.0.0.2, 10.0.0.1'
+    })
+    assert.deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+  })
+
+  it('should skip blank entries', function () {
+    var req = createReq('127.0.0.1', {
+      'x-forwarded-for': '10.0.0.2,, 10.0.0.1'
+    })
+    assert.deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+  })
+
+  it('should trim leading OWS', function () {
+    var req = createReq('127.0.0.1', {
+      'x-forwarded-for': ' 10.0.0.2 ,  , 10.0.0.1 '
+    })
+    assert.deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+  })
+})
+
+function createReq (socketAddr, headers) {
+  return {
+    connection: {
+      remoteAddress: socketAddr
+    },
+    headers: headers || {}
+  }
+}
